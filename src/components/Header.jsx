6d385b8bbde7ed22d667ccda3ded40f9a7f4e170
adityasharma0131 <adityasharma0431@gmail.com>
2024-07-26 12:53:29 +0530

    modification to header component  and hero section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,93 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import logo from "../assets/Screenshot_2024-06-25_143958-removebg-preview 2.png";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { NavLink, Link } from "react-router-dom";
+import { RiArrowRightUpLine, RiCloseLine, RiMenuLine } from "react-icons/ri";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isScrollingDown, setIsScrollingDown] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-  useEffect(() => {
-    let lastScrollY = window.scrollY;
+  const handleToggle = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    const handleScroll = () => {
-      setIsScrollingDown(window.scrollY > lastScrollY);
-      lastScrollY = window.scrollY;
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-  const closeMenu = () => setIsMenuOpen(false);
+  const handleClose = () => {
+    setMenuOpen(false);
+  };
 
   return (
-    <header className={isScrollingDown ? "hidden" : ""}>
-      <div className="logo">
-        <Link to="/">
-          <img src={logo} alt="Logo" />
-        </Link>
-      </div>
-      <input
-        type="checkbox"
-        id="nav_check"
-        hidden
-        checked={isMenuOpen}
-        readOnly
-      />
-      <nav className={isMenuOpen ? "open" : ""}>
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? "active" : "")}
-              onClick={closeMenu}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/products"
-              className={({ isActive }) => (isActive ? "active" : "")}
-              onClick={closeMenu}
-            >
-             About us
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/gallery"
-              className={({ isActive }) => (isActive ? "active" : "")}
-              onClick={closeMenu}
-            >
-              Services
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/about-us"
-              className={({ isActive }) => (isActive ? "active" : "")}
-              onClick={closeMenu}
-            >
-              Get in Touch
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/contact-us"
-              className={({ isActive }) => (isActive ? "active" : "")}
-              onClick={closeMenu}
-            >
-              Q & A
-            </NavLink>
-          </li>
-        </ul>
+    <header className="header" id="header">
+      <nav className="nav container">
+        <img className="nav__logo" src={logo} alt="Logo" />
+
+        <div
+          className={`nav__menu ${menuOpen ? "show-menu" : ""}`}
+          id="nav-menu"
+        >
+          <ul className="nav__list">
+            {["Home", "About Us", "Projects", "Studio", "Contact"].map(
+              (item, index) => (
+                <li className="nav__item" key={index}>
+                  <a href="#" className="nav__link" onClick={handleClose}>
+                    <RiArrowRightUpLine />
+                    <span>{item}</span>
+                  </a>
+                </li>
+              )
+            )}
+          </ul>
+
+          <div className="nav__close" id="nav-close" onClick={handleClose}>
+            <RiCloseLine />
+          </div>
+        </div>
+
+        <div className="nav__toggle" id="nav-toggle" onClick={handleToggle}>
+          <RiMenuLine />
+        </div>
       </nav>
-      <label htmlFor="nav_check" className="hamburger" onClick={toggleMenu}>
-        {isMenuOpen ? <FaTimes className="close-icon" /> : <FaBars className="hamburger-icon" />}
-      </label>
     </header>
   );
 };
